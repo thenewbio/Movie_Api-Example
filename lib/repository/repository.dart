607800aeb84335ecb4f9e3b4b23399eade370559
innocent <okamainnocent2020@gmail.com>
2022-04-23 17:04:33 -0700

    second commit

---
+++ lib/repository/repository.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mini_practice/provider/environment.dart';
+import 'package:mini_practice/provider/movie_exception.dart';
+import 'package:mini_practice/views/movie.dart';
+
+final movieServiceProvider = Provider<MovieService>((ref) {
+  final config = ref.watch(provider);
+
+  return MovieService(config, Dio());
+});
+
+class MovieService {
+  MovieService(this._environmentConfig, this._dio);
+
+  final EnvironmentConfig _environmentConfig;
+  final Dio _dio;
+
+  Future<List<Movie>> getMovies() async {
+    try {
+      final response = await _dio.get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=${_environmentConfig.movieApiKey}&language=en-US&page=1",
+      );
+
+      final results = List<Map<String, dynamic>>.from(response.data['results']);
+
+      List<Movie> movies = results
+          .map((movieData) => Movie.fromMap(movieData))
+          .toList(growable: false);
+      return movies;
+    } on DioError catch (dioError) {
+      throw MoviesException.fromDioError(dioError);
+    }
+  }
+}
